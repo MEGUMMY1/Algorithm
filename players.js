fcb72b39c69e1d167f3cd53cdf2e439ad6d51c86
HyeJean <57613101+MEGUMMY1@users.noreply.github.com>
2023-09-30 19:30:23 +0900

    Update players.js

--- players.js
+++ players.js
@@ -1,4 +1,4 @@
-function solution(players, callings) {
+/*function solution(players, callings) {
     var answer = players;
     let temp = "";
     
@@ -13,4 +13,19 @@ function solution(players, callings) {
     }
     
     return answer;
+}*/
+function solution(players, callings) {
+    let maps = {};
+    for(let i = 0; i < players.length; i++){
+        maps[players[i]] = i;
+    }
+    callings.forEach(v => {
+        let num = maps[v];
+        let temp = players[num-1];
+        players[num-1] = v;
+        players[num] = temp;
+        maps[v]--;
+        maps[players[num]]++;
+    })
+    return Object.entries(maps).sort((a,b) => a[1] - b[1]).map(v => v[0]);
 }
